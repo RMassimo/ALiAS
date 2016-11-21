4a1f8b3a5baced06e74321e22216f473fdce7419
RMassimo <r.massimo@grr.la>
2016-11-21 17:43:29 +0100

    Aggiunta Menu a Tendina & prima Main Form.

--- Alias/Alias/mainForm.Designer.cs
+++ Alias/Alias/mainForm.Designer.cs
@@ -33,13 +33,17 @@
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flpGames = new System.Windows.Forms.FlowLayoutPanel();
+            this.gamesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menu.SuspendLayout();
             this.SuspendLayout();
             // 
             // menu
             // 
             this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.gamesToolStripMenuItem});
             this.menu.Location = new System.Drawing.Point(0, 0);
             this.menu.Name = "menu";
             this.menu.Size = new System.Drawing.Size(607, 24);
@@ -59,27 +63,52 @@
             // 
             this.infoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("infoToolStripMenuItem.Image")));
             this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
-            this.infoToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.infoToolStripMenuItem.Text = "Info";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("exitToolStripMenuItem.Image")));
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // flpGames
+            // 
+            this.flpGames.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpGames.Location = new System.Drawing.Point(0, 24);
+            this.flpGames.Name = "flpGames";
+            this.flpGames.Size = new System.Drawing.Size(607, 350);
+            this.flpGames.TabIndex = 1;
+            // 
+            // gamesToolStripMenuItem
+            // 
+            this.gamesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem});
+            this.gamesToolStripMenuItem.Name = "gamesToolStripMenuItem";
+            this.gamesToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.gamesToolStripMenuItem.Text = "Games";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(607, 374);
+            this.Controls.Add(this.flpGames);
             this.Controls.Add(this.menu);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menu;
             this.Name = "mainForm";
             this.Text = "ALiAS - Version 0.0.1.1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.main_Load);
             this.menu.ResumeLayout(false);
             this.menu.PerformLayout();
@@ -94,5 +123,8 @@
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel flpGames;
+        private System.Windows.Forms.ToolStripMenuItem gamesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
     }
 }
--- Alias/Alias/mainForm.cs
+++ Alias/Alias/mainForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics;
 
 namespace Alias
 {
@@ -17,12 +18,92 @@ namespace Alias
         //  *** VARIABILI GLOBALI ***
 
         public string root = "C:/ProgramData/Hydra-Studio/Alias/";
+        public string[] games = new string[65536];
+        public int index = 0;
+        public int width = 230, height = 107;
+        public List<string> libraryGames = new List<string>();
 
         //  *** INIZIO CODICE ***
 
         public mainForm()
         {
             InitializeComponent();
+        }
+
+        public void UpdateGames()
+        {
+            // Creo un array da un elemento per contenere il contenuto del file *root*/User/path.usr
+            string[] steampath = new string[3];
+            // Ottengo la path di steam
+            steampath = File.ReadAllLines(root + "/path.usr", Encoding.UTF8);
+            // Trovo tutti gli appmanifest nella steampath
+            for (int paths = 0; paths < 3; paths++)
+            {
+                if (steampath[paths] != "")
+                {
+                    string[] appmanifest = System.IO.Directory.GetFiles(steampath[paths]);
+                    // Resetto l' indice
+                    index = 0;
+                    // Cicla le appmanifest_%id% e le mette nell' array games
+                    for (int i = 0; i < appmanifest.Length; i++)
+                    {
+                        if (appmanifest[i].StartsWith(steampath[paths] + "\appmanifest_"))
+                        {
+                            string fileName = appmanifest[i];
+                            games[index] = fileName.Substring(11);
+                            index++;
+                        }
+                    }
+                    // E aggiunge i nuovi giochi nella libreria
+                    for (int i = 0; i < appmanifest.Length; i++)
+                    {
+                        try
+                        {
+                            appmanifest[i] = appmanifest[i].Substring(appmanifest[i].LastIndexOf('_') + 1);
+                            appmanifest[i] = appmanifest[i].Remove(appmanifest[i].LastIndexOf(".acf"));
+                            //VECCHIO METODO ->     lbxLibrary.Items.Add("steam://rungameid/" + appmanifest[i]);
+                            libraryGames.Add("steam://rungameid/" + appmanifest[i]);
+                        }
+                        catch
+                        {
+                            // Niente
+                        }
+                    }
+                }
+            }
+        }
+
+        public void RefreshGames()
+        {
+            for (int i = 0; i < libraryGames.Count; i++)
+            {
+                // Inizializza la foto dei giochi
+                PictureBox game = new PictureBox();
+                game.Parent = flpGames;
+                game.Click += gameLaunch;
+                // Modifica alcune sue proprieta'
+                game.Name = "game_" + i;
+                // VECCHIO METODO ->    game.Tag = lbxLibrary.Items[i].ToString();
+                game.Tag = libraryGames[i];
+                game.Width = 230;
+                game.Height = 107;
+                game.SizeMode = PictureBoxSizeMode.StretchImage;
+                // Ricava lo steam Id
+                // VECCHIO METODO ->    string gameName = lbxLibrary.Items[i].ToString();
+                string gameName = libraryGames[i];
+                string steamid = gameName.Substring(gameName.LastIndexOf("/") + 1);
+                // Carica immagine
+                game.Load("http://cdn.akamai.steamstatic.com/steam/apps/" + steamid + "/header.jpg"); ;
+            }
+        }
+
+        public void gameLaunch(object sender, EventArgs e)
+        {
+            PictureBox game = (PictureBox)sender;
+            Process p = new Process();
+            // Starta il processo scritto nel tag
+            p.StartInfo = new ProcessStartInfo(game.Tag.ToString());
+            p.Start();
         }
 
         private void main_Load(object sender, EventArgs e)
@@ -33,13 +114,19 @@ namespace Alias
                 chooseFolders chooseFolders = new chooseFolders();
                 chooseFolders.ShowDialog();
             } else {    // Altrimenti se ha almeno una volta avviato ALiAS
-                
+                // DO-NOTHING
             }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
-        }        
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UpdateGames();
+            RefreshGames();
+        }
     }
 }
